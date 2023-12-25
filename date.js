327c874903bdf2edc751ab54737a0effe37c2576
sgyanendra <154693868+sgyanendra@users.noreply.github.com>
2023-12-25 14:13:58 +0000

    date time

---
+++ date.js
@@ -0,0 +1,13 @@
+//let myDate=new Date();
+// console.log(myDate);
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+
+// let myCreateDate=new Date(2023,0,23,5,3);
+// let myCreateDate=new Date("01-14-2022");
+// console.log(myCreateDate.toLocaleString());
+
+let myDate=new Date();
+console.log(myDate.getDay());
+console.log(myDate.getMonth());

de348c32604c94a2824742d4b2e79edc37564cdd
sgyanendra <154693868+sgyanendra@users.noreply.github.com>
2023-12-26 18:09:23 +0000

    array

---
+++ arr1.js
@@ -0,0 +1,19 @@
+const arr1=[1,2,3,4,5]
+// arr1.push(0,9);
+// console.log(arr1);
+// arr1.unshift(11);
+// console.log(arr1);
+// arr1.pop()
+// console.log(arr1);
+// arr1.shift();
+// console.log(arr1);
+// console.log(arr1.indexOf(3));
+// console.log(arr1.includes(9));
+// let arr2=arr1.slice(1,3);
+// console.log(arr2);
+// let arr3=arr1.splice(1,3);
+// console.log(arr3);
+
+const arr2=["Hello","Frinds",['A','B','C']];
+// console.log(arr1.concat(arr2))
+console.log(arr2.flat())
